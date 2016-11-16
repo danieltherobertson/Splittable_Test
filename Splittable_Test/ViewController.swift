51616a190f02304db19103e147d9839375609b4a
Daniel Robertson <danieltherobertson@gmail.com>
2016-11-16 11:19:29 +0000

    Initial commit - project setup

---
+++ Splittable_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Splittable_Test
+//
+//  Created by Daniel Robertson on 16/11/2016.
+//  Copyright © 2016 Daniel Robertson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
